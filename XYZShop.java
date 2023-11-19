d3a58e66f873fbf9d09bbaea30bbb4e57b5b087f
AhmedAli <ahmedalialgadal77@gmail.com>
2023-11-19 23:42:49 +0300

    create Register() and change c,s to type User

--- XYZShop.java
+++ XYZShop.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+
 public class XYZShop {
     String name;
-    Customer c;
-    Seller s;
+    User c;
+    User s;
     Scanner sc1;
     Scanner sc2;
 
@@ -13,7 +14,6 @@ public class XYZShop {
         sc2 = new Scanner(System.in);
     }
 
-
     void getStaring() {
         System.out.println("Register as: ");
         System.out.println("1.Customer  2.Seller");
@@ -23,34 +23,9 @@ public class XYZShop {
                 int userType = Integer.parseInt(option);
                 if (userType == 1 || userType == 2) {
                     if (userType == 1) {
-                        System.out.println("Enter your name:");
-                        c.setuName(sc2.nextLine());
-                        System.out.println("Enter your Email:");
-                        //we must check the email format before we set it and must not be empty
-                        String uMail = sc2.nextLine();
-                        if (uMail.contains(" ") || !(uMail.contains("@") && uMail.contains("."))) {
-                            System.out.println(" You should provide a correct email address format. Example: name@email.com");
-                            return;
-                        }
-                        c.setuEmail(uMail);
-
-                        System.out.println("Enter your PassWord:");
-                        //must check also the password, cannot be separated by space or be empty
-                        String uPassword = sc2.nextLine();
-                        if (uPassword.trim().contains(" ")) {
-                            System.out.println("You should provide a correct password format. Example: 'Password123'. Not: 'Password 123'.");
-                            return;
-                        }
-                        c.setuPassword(uPassword.trim());
-                        System.out.println("Welcome " + c.getuName() + "! " + "Your username is: " + c.getuEmail() + " your password is: " + c.getuPassword());
+                        Register(c);
                     } else if (userType == 2) {
-                        System.out.println("Enter your name:");
-                        s.setuName(sc2.nextLine());
-                        System.out.println("Enter your Email:");
-                        s.setuEmail(sc2.nextLine());
-                        System.out.println("Enter your PassWord:");
-                        s.setuPassword(sc2.nextLine());
-                        System.out.println("Welcome " + s.getuName() + "! " + "Your username is: " + s.getuEmail());
+                        Register(s);
                     }
                     break;
                 } else {
@@ -61,5 +36,36 @@ public class XYZShop {
             }
 
         }
+    }
+
+    void Register(User u) {
+        System.out.println("Enter your name:");
+        String uName = sc2.nextLine();
+        if(uName.trim().isEmpty()){
+            System.out.println("Your name must not be null");
+            return;
+        }
+        u.setuName(uName.trim());
+        System.out.println("Enter your Email:");
+        // we must check the email format before we set it and must not be empty
+        String uMail = sc2.nextLine();
+        if (uMail.contains(" ") || !(uMail.contains("@") && uMail.contains("."))) {
+            System.out.println(" You should provide a correct email address format. Example: name@email.com");
+            return;
+        }
+        u.setuEmail(uMail);
+
+        System.out.println("Enter your PassWord:");
+        // must check also the password, cannot be separated by space or be empty
+        String uPassword = sc2.nextLine();
+        if (uPassword.trim().contains(" ")) {
+            System.out.println(
+                    "You should provide a correct password format. Example: 'Password123'. Not: 'Password 123'.");
+            return;
+        }
+        u.setuPassword(uPassword.trim());
+        System.out.println("Welcome " + u.getuName() + "! " + "Your username is: " + u.getuEmail()
+                + " your password is: " + u.getuPassword());
+
     }
 }
