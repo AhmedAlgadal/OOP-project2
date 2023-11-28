972dcc8b202239f4a72683fd51735333f8e8771c
Ahmed Algadal <150537587+AhmedAlgadal@users.noreply.github.com>
2023-11-28 14:53:49 +0300

    case 1,3,4

--- Customer.java
+++ Customer.java
@@ -8,19 +8,21 @@ public class Customer extends User {
     private ArrayList<Item> boughtItem;
     private Scanner scanner;
     private String selection;
-    // private ItemCollections boughtCollections;
+    private ItemCollections itemColl;
     private Invoice invoice;
 
     public Customer() {
         boughtItem = new ArrayList<Item>();
         scanner = new Scanner(System.in);
+        itemColl= new ItemCollections();
     }
 
     public Customer(int uId, String uName, String uEmail, String uPassword, boolean isActive, double balance) {
-        super(uId, uName, uEmail, uPassword, isActive);
+        super(uId, uName, uEmail, uPassword, isActive, balance);
         // this.balance= balance;
         boughtItem = new ArrayList<Item>();
         scanner = new Scanner(System.in);
+        itemColl= new ItemCollections();
     }
 
     public void showCustomerBoard() {
@@ -40,7 +42,7 @@ public class Customer extends User {
             switch (selection.trim()) {
                 case "1":
                     // --------------
-                    System.out.println("case 1");
+                    displayOrders();
 
                     break;
                 case "2":
@@ -50,11 +52,11 @@ public class Customer extends User {
                     break;
                 case "3":
                     // ------------------
-                    System.out.println("case 3");
+                    deleteOrder();
                     break;
                 case "4":
                     // ------------------
-                    System.out.println("case 4");
+                    displayBalance();
                     break;
 
                 case "5":
@@ -87,6 +89,23 @@ public class Customer extends User {
     // this.balance = balance;
     // }
 
+    public void displayOrders() {
+        if (!getBoughtItem().isEmpty()) {
+            System.out.println("Your Orders:");
+            int index = 1;
+            for (Item item : getBoughtItem()) {
+                System.out.println("#" + index + " Item Name: " + item.getItemName() + " Item price: "
+                        + item.getPrice() +
+                        " Item quantity: " + item.getQuantity());
+                System.out.println("===================================================================");
+                index++;
+
+            }
+        } else {
+            System.out.println("Your Cart is Empty.");
+        }
+    }
+
     public void buyProducts() {
         // -- update the balance and item qut and Seller money ++.
         ItemCollections.displayApproveItems();
@@ -116,7 +135,7 @@ public class Customer extends User {
             System.out.println("your Item is: " + ItemCollections.getApproveItems(s - 1).getItemName());
             if (quantity > 0 && quantity <= ItemCollections.getApproveItems(s - 1).getQuantity()) {
                 double cost = quantity * ItemCollections.getApproveItems(s - 1).getPrice();
-                if (cost <= getuBalance()) {
+                if (cost <= this.getuBalance()) {
                     Item requiredItem = new Item();
                     requiredItem.setItemNo(ItemCollections.getApproveItems(s - 1).getItemNo());
                     requiredItem.setItemName(ItemCollections.getApproveItems(s - 1).getItemName());
@@ -127,8 +146,10 @@ public class Customer extends User {
                     ItemCollections.getApproveItems(s - 1)
                             .setQuantity(ItemCollections.getApproveItems(s - 1).getQuantity() - quantity);
                     System.out.println("The payment is successfully done!");
+                    System.out.println("Now your Balance is: " + this.getuBalance());
                 } else {
                     System.out.println("Insufficent balance!");
+                    displayBalance();
                 }
             } else {
                 System.out.println("Sorry, it is out of stock!");
@@ -140,21 +161,87 @@ public class Customer extends User {
 
     }
 
-    public void deleteOrder(Item item) {
+    public void deleteOrder() {
         // ++ update the balance and item qut and Seller money --.
-        this.boughtItem.remove(item);
+        displayOrders();
+        System.out.println("choose Order to delete:");
+        int order;
+        String o = scanner.nextLine().trim();
+        // try and catch block to handle the String input .
+        try {
+            order = Integer.parseInt(o);
+        } catch (Exception e) {
+            System.out.println("You have to enter an integer number.");
+            return;
+        }
+        System.out.println("Enter the Quantity of your Item:");
+        int quantity;
+        String q = scanner.nextLine().trim();
+        // try and catch block to handle the String input .
+        try {
+            quantity = Integer.parseInt(q);
+        } catch (Exception e) {
+            System.out.println("You have to enter an integer number.");
+            return;
+        }
+
+        if (getBoughtItem().size() >= order && order > 0) {
+
+            if (getBoughtItem(order - 1).getQuantity() >= quantity) {
+
+                if (getBoughtItem(order - 1).getQuantity() == quantity) {
+                    System.out.println(getBoughtItem(order - 1).getItemName() + " is now removed");
+                    // update balance after delete order
+                    this.setuBalance(getuBalance() + (quantity * getBoughtItem(order - 1).getPrice()));
+                    removeBoughtItem(order - 1);
+                } else {
+                    getBoughtItem(order - 1).setQuantity(getBoughtItem(order - 1).getQuantity() - quantity);
+                    // update balance after delete order
+                    this.setuBalance(getuBalance() + (quantity * getBoughtItem(order - 1).getPrice()));
+                    
+                    
+                    System.out.println(getBoughtItem(order - 1).getItemName() + " is update now.");
+                }
+            } else {
+                System.out.println("Wrong Quantity.");
+            }
+        } else {
+            System.out.println("this product does not exist");
+        }
+
     }
 
-    public void displayProducts() {
-        int index = 1;
-        for (Item item : boughtItem) {
-            if (item.getQuantity() == 0) {
-                System.out.println("#" + index + " " + item.getItemName() + " out of stock");
-            } else {
-                System.out.println("#" + index + " " + item.getItemName());
+    // public void displayProducts() {
+    // int index = 1;
+    // for (Item item : boughtItem) {
+    // if (item.getQuantity() == 0) {
+    // System.out.println("#" + index + " " + item.getItemName() + " out of stock");
+    // } else {
+    // System.out.println("#" + index + " " + item.getItemName());
+    // }
+    // index++;
+    // }
+    // }
+
+    public void displayBalance() {
+        System.out.println("Now your Balance is: " + this.getuBalance());
+        System.out.println("Do you want to add to your Balance (y/n):");
+        String answer = scanner.nextLine();
+        if (answer.equals("y") || answer.equals("Y")) {
+            System.out.println("Enter your update:");
+            double balance;
+            String b = scanner.nextLine().trim();
+            // try and catch block to handle the String input .
+            try {
+                balance = Double.parseDouble(b);
+            } catch (Exception e) {
+                System.out.println("You have to enter an integer number.");
+                return;
             }
-            index++;
+            this.setuBalance(balance + getuBalance());
+            System.out.println("Now your Balance is: " + this.getuBalance());
         }
+
     }
 
     public void addBoughtItem(Item boughtItem) {
@@ -163,6 +250,14 @@ public class Customer extends User {
 
     public ArrayList<Item> getBoughtItem() {
         return boughtItem;
+    }
+
+    public void removeBoughtItem(int itemNo) {
+        getBoughtItem().remove(getBoughtItem().get(itemNo));
+    }
+
+    public Item getBoughtItem(int n) {
+        return boughtItem.get(n);
     }
 
 }
